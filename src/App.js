7f0845fc6026f63163de94e1108aef8b03635801
Abhishek Tomar <itsabhi.at@gmail.com>
2021-08-18 06:39:23 +0530

    2.testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h3>hello this is react</h3>
+      <h3>hello this is testing</h3>
+      <h3>hello this is testing</h3>
     </div>
   );
 }

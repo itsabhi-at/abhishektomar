14e93b7dce78611f9572d9c252ca12cf13e8e055
Abhishek Tomar <itsabhi.at@gmail.com>
2021-08-20 21:57:38 +0530

    3.Portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import styles
+
+import "./styles/app.scss";
+// import modules
+
+import { Route, Switch } from "react-router-dom";
+
+// import pages
+import AboutUs from "./pages/AboutUs";
+import Navbar from "./components/Navbar";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
     <div className="App">
-      <h3>hello this is testing</h3>
-      <h3>hello this is testing</h3>
+      <Switch>
+        <Route path="/portfolio">
+          <Navbar />
+          <Portfolio />
+        </Route>
+        <Route path="/">
+          <AboutUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+import Navbar from "../components/Navbar";
+import ServicesSection from "../components/ServicesSection";
+const AboutUs = () => {
+  return (
+    <>
+      <Navbar />
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
+};
+export default AboutUs;
